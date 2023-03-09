a52b53c6cd4cdbf3359c649e393b7734423d7366
dinarakurmanbek <dinara.kurmanbek@stud.fra-uas.de>
2023-03-09 23:09:00 +0600

    added form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {EmpAddEditComponent} from "./emp-add-edit/emp-add-edit.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'crud-app';
+  constructor(private _dialog: MatDialog) {
+  }
+
+  openAddEditEmpForm() {
+    this._dialog.open(EmpAddEditComponent)
+  }
 }
